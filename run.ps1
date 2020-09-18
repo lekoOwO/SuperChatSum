5eecb85453e0f9ed8cf0d605d71a67978604fd7e
showgan <rexx0520@gmail.com>
2020-09-18 18:24:37 +0800

    維持向下相容性

--- run.ps1
+++ run.ps1
@@ -1,13 +1,22 @@
-$config = Get-Content 'config.ini' | ConvertFrom-StringData
+$env:PYTHONIOENCODING = "utf-8"
+
+$config = Get-Content (Join-Path $PSScriptRoot 'config.ini')  | ConvertFrom-StringData
+
+$output_dir = Join-Path $PSScriptRoot "output"
+If(!(Test-Path $output_dir))
+{
+    New-Item -ItemType Directory -Force -Path $output_dir | Out-Null
+}
 
 $uuid = [Guid]::NewGuid().Guid
-$rawDataFilename = $uuid + ".json"
-$calculatedFilename = $uuid + ".calc.json"
+
+$rawDataFilepath = Join-Path $output_dir ($uuid + ".json")
+$calculatedFilepath = Join-Path $output_dir ($uuid + ".calc.json")
 
 $URL = $Args[0]
 
 Write-Output ("UUID: " + $uuid)
 
-python (Join-Path $PSScriptRoot "chat-replay-downloader/chat_replay_downloader.py") $URL -message_type superchat -output (Join-Path $PSScriptRoot $rawDataFilename) | Out-Null
+python (Join-Path $PSScriptRoot "chat-replay-downloader/chat_replay_downloader.py") $URL -message_type superchat -output $rawDataFilepath | Out-Null
 
-python (Join-Path $PSScriptRoot "process.py") $config.CURRENCYLAYER_API_KEY (Join-Path $PSScriptRoot $rawDataFilename) (Join-Path $PSScriptRoot $calculatedFilename) -c (Join-Path $PSScriptRoot $config.CURRENCY_FILE_NAME)
+python (Join-Path $PSScriptRoot "process.py") $config.CURRENCYLAYER_API_KEY $rawDataFilepath $calculatedFilepath -c (Join-Path $PSScriptRoot $config.CURRENCY_FILE_NAME)
